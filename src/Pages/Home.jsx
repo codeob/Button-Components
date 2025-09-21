37c77e7548c9ca91c258e0a1865d3a872c656812
Obed Tawiah <tobed1586@gmail.com>
2025-09-21 23:02:48 +0000

    background img

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,11 @@
-
+import bgimg from '../assets/bgimg.jpg'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-cover text-white h-screen w-screen flex justify-center items-center  bg-gradient-to-t  from-gray-900 via-slate-900 to-black" >
+            <div className='flex justify-center items-center border-2 h-fit w-fit '>
+
+            </div>
+    </div>
   )
 }
